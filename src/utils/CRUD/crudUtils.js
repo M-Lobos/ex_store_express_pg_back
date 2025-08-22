6bc91b19739378f12f5f67bcecf7269e4582bfdb
M-Lobos <manuel.lobos.88@gmail.com>
2025-08-22 18:49:51 -0400

    feat: create metod on model Usuario working fine

--- src/utils/CRUD/crudUtils.js
+++ src/utils/CRUD/crudUtils.js
@@ -1,16 +1,18 @@
 import { InternalServerError } from "../../errors/TypesOfErrors.js";
+import { query } from "../../config/db.config.js";
 
 /**
  * Crea un nuevo registro en una tabla específica a través de un objeto
- * @param {string } tableName - Nombre de la tabla en la base de datos.
- * @param {object} data - Datos que se envían para registro en tabla de nombre tableName
- * @returns {Promise<Object>} - Retorna el registro creado en formato Objeto
+ * @param {string} tableName - Nombre de la tabla en la base de datos.
+ * @param {object} data - Datos en formato de objeto que se envían para registro en tabla de nombre tableName
+ * @returns {Promise<object>} - Retorna el registro creado en formato Objeto
  */
 
 export const createRecord = async (tableName, data) => {
+
     try {
-        const columns = Object.keys(data);                          //devuelve un array con las key de un objeto dado
-        const values = Object.values(data);                         //devuelve un array con los values de un objeto dado
+        const columns = Object.keys(data);                       //devuelve un array con las key de un objeto dado
+        const values = Object.values(data);              //devuelve un array con los values de un objeto dado
         const placeholders = columns.map((_, i) => `$${i + 1}`)     //Se transforman los datos para que lleguen las
         //posiciones parametrizadas
 
@@ -19,7 +21,6 @@ export const createRecord = async (tableName, data) => {
             VALUES (${placeholders.join(`, `)})
             RETURNING *
         `
-
         const { rows } = await query(insertQuery, values);
         return rows[0];
 
